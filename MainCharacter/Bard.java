568720685c0204de350c84cf0ea78fb1102f7710
Kirsten98 <42506533+Kirsten98@users.noreply.github.com>
2019-10-04 15:53:24 -0700

    Extended Bard with CharacterClass and removed methods listed in CharacterClass

--- MainCharacter/Bard.java
+++ MainCharacter/Bard.java
@@ -18,7 +18,7 @@ import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Vector;
 
-public class Bard {
+public class Bard extends CharacterClass{
 
     public Bard(CharacterSheet character) {
         this.character = character;
@@ -56,197 +56,6 @@ public class Bard {
     Vector<String> allNinthLevelSpells = Spells.allClassSpellSetUp(9);
 
     Vector<String> proficiencies = new Vector<>();
-
-
-    // Tested and verfied 9/10
-
-    /**
-     * @param character       CharacterSheet that needs the Ability Modifier added to
-     * @param AbilityChoice   Ability choice that the modifier is adding to. 1.) Charisma 2.) Strength 3.)Dexterity 4.) Wisdom 5.)Intelligence 6.) Constitution
-     * @param modifierAddtion The Value that is getting added to the Modifier
-     */
-    public void abilityAddition(CharacterSheet character, String AbilityChoice, int modifierAddtion) {
-        if (AbilityChoice.equals("Charisma")) {
-            character.setCharismaScore(character.getCharismaScore() + modifierAddtion);
-            System.out.println("Charisma +" + modifierAddtion);
-            character.setCharismaMod(findAbilityMod(character.getCharismaScore()));
-
-        }
-        if (AbilityChoice.equals("Strength")) {
-            character.setStrengthScore(character.getStrengthScore() + modifierAddtion);
-            System.out.println("Strength +" + modifierAddtion);
-            character.setStrengthMod(findAbilityMod(character.getStrengthScore()));
-
-        }
-        if (AbilityChoice.equals("Dexterity")) {
-            character.setDexterityScore(character.getDexterityScore() + modifierAddtion);
-            System.out.println("Dexterity +" + modifierAddtion);
-            character.setDexterityMod(findAbilityMod(character.getDexterityScore()));
-
-        }
-        if (AbilityChoice.equals("Wisdom")) {
-            character.setWisdomScore(character.getWisdomScore() + modifierAddtion);
-            System.out.println("Wisdom +" + modifierAddtion);
-            character.setWisdomMod(findAbilityMod(character.getWisdomScore()));
-        }
-        if (AbilityChoice.equals("Intelligence")) {
-            character.setIntelligenceScore(character.getIntelligenceScore() + modifierAddtion);
-            System.out.println("Intelligence +" + modifierAddtion);
-            character.setIntelligenceMod(findAbilityMod(character.getIntelligenceScore()));
-        }
-        if (AbilityChoice.equals("Constitution")) {
-            character.setConstitutionScore(character.getConstitutionScore() + modifierAddtion);
-            System.out.println("Constitution +" + modifierAddtion);
-            character.setConstitutionMod(findAbilityMod(character.getConstitutionScore()));
-
-        }
-    }
-
-    /**
-     * Gives the user the  choice to either add +2 to 1 ability score, or add 2 separate ability scores by +1
-     *
-     * @param addLevelStage Stage that the choice will be displayed on
-     * @param maxLevel      Maximum level that the user has selected
-     * @param startinglevel Level that the user is currently making choices for
-     * @return
-     */
-    public GridPane abilityScoreImprovement(Stage addLevelStage, int maxLevel, int startinglevel) {
-        Button continueButton = new Button("Continue");
-        Label choice = new Label();
-        choice.setWrapText(true);
-        Button charisma = new Button("Charisma");
-        Button strength = new Button("Strength");
-        Button dexterity = new Button("Dexterity");
-        Button wisdom = new Button("Wisdom");
-        Button intelligence = new Button("Intelligence");
-        Button constitution = new Button("Constitution");
-
-        GridPane pane = new GridPane();
-        Label question = new Label("Would you like to increase one ability score \nby +2 or two ability scores by +1 ");
-        Button plus2 = new Button("One ability score by +2");
-        Button plus1 = new Button("Two ability scores by +1");
-
-        pane.add(question, 0, 0, 6, 1);
-        pane.add(plus2, 1, 1);
-        pane.add(plus1, 1, 2);
-        pane.setVgap(20);
-        pane.setHgap(20);
-
-        charisma.setOnAction(charismaEvent -> {
-            if (character.getCharismaScore() + 1 > 20 || character.getCharismaScore() + 2 > 20) {
-                choice.setText("Charisma ability score is greater than 20.\nPlease choose a different option.");
-                continueButton.setDisable(true);
-            } else {
-                choice.setText("Charisma");
-                continueButton.setDisable(false);
-            }
-        });
-
-        strength.setOnAction(strengthEvent -> {
-            if (character.getStrengthScore() + 1 > 20 || character.getStrengthScore() + 2 > 20) {
-                choice.setText("Strength ability score is greater than 20. \nPlease choose a different option.");
-                continueButton.setDisable(true);
-            } else {
-                choice.setText("Strength");
-                continueButton.setDisable(false);
-            }
-        });
-        dexterity.setOnAction(dexterityEvent -> {
-            if (character.getDexterityScore() + 1 > 20 || character.getDexterityScore() + 2 > 20) {
-                choice.setText("Dexterity ability score is greater than 20. \nPlease choose a different option.");
-                continueButton.setDisable(true);
-            } else {
-                choice.setText("Dexterity");
-                continueButton.setDisable(false);
-            }
-        });
-        wisdom.setOnAction(wisdomEvent -> {
-            if (character.getWisdomScore() + 1 > 20 || character.getWisdomScore() + 2 > 20) {
-                choice.setText("Wisdom ability score is greater than 20. \nPlease choose a different option.");
-                continueButton.setDisable(true);
-            } else {
-                choice.setText("Wisdom");
-                continueButton.setDisable(false);
-            }
-        });
-        intelligence.setOnAction(intelligenceEvent -> {
-            if (character.getIntelligenceScore() + 1 > 20 || character.getIntelligenceScore() + 2 > 20) {
-                choice.setText("Intelligence ability score is greater than 20. \nPlease choose a different option.");
-                continueButton.setDisable(true);
-            } else {
-                choice.setText("Intelligence");
-                continueButton.setDisable(false);
-            }
-        });
-        constitution.setOnAction(constitutionEvent -> {
-            if (character.getConstitutionScore() + 1 > 20 || character.getConstitutionScore() + 2 > 20) {
-                choice.setText("Constitution ability score is greater than 20. \nPlease choose a different option.");
-                continueButton.setDisable(true);
-            } else {
-                choice.setText("Constitution");
-                continueButton.setDisable(false);
-            }
-        });
-
-
-        plus2.setOnAction(e -> {
-            pane.getChildren().remove(plus1);
-            pane.getChildren().remove(plus2);
-            pane.add(charisma, 0, 1);
-            pane.add(strength, 1, 1);
-            pane.add(dexterity, 2, 1);
-            pane.add(wisdom, 0, 2);
-            pane.add(intelligence, 1, 2);
-            pane.add(constitution, 2, 2);
-            pane.add(choice, 1, 3, 6, 1);
-            pane.add(continueButton, 1, 5);
-            question.setText("Which Ability score would you like to increase by +2 ?");
-
-            continueButton.setOnAction(continueEvent -> {
-                abilityAddition(character, choice.getText(), 2);
-                if (startinglevel == maxLevel) {
-                    addLevelStage.close();
-                } else addLevel(addLevelStage, maxLevel, startinglevel + 1);
-//                    addLevel(addLevelStage, maxLevel, startinglevel + 1);
-            });
-
-        });
-
-        plus1.setOnAction(e -> {
-            String[] intChoice = new String[2];
-            pane.getChildren().remove(plus1);
-            pane.getChildren().remove(plus2);
-            pane.add(charisma, 0, 1);
-            pane.add(strength, 1, 1);
-            pane.add(dexterity, 2, 1);
-            pane.add(wisdom, 0, 2);
-            pane.add(intelligence, 1, 2);
-            pane.add(constitution, 2, 2);
-            pane.add(choice, 1, 3, 6, 1);
-            pane.add(continueButton, 1, 4);
-            continueButton.setDisable(true);
-            question.setText("Choose your first ability to increase by +1.");
-
-            continueButton.setOnAction(continueEvent -> {
-                intChoice[0] = choice.getText();
-                question.setText("Choose your second ability to increase by +1.");
-                continueButton.setDisable(true);
-                choice.setText("");
-
-                continueButton.setOnAction(continueEvent2 -> {
-                    intChoice[1] = choice.getText();
-                    for (int i = 0; i < 2; i++) {
-                        abilityAddition(character, intChoice[i], 1);
-                        if (startinglevel == maxLevel) {
-                            addLevelStage.close();
-                        } else addLevel(addLevelStage, maxLevel, startinglevel + 1);
-                    }
-                });
-            });
-
-        });
-        return pane;
-    }
 
     /**
      * Updates a VBox that will provide a HP label with current character HP, proficiency label with current proficiency modifier, a list view of the level proficiencies, and a list view of the features.
@@ -667,7 +476,7 @@ public class Bard {
                         secondLevelSpells.remove(secondLevelSpellListView.getItems().get(i).getText());
                     }
                 }
-                borderPane.setCenter(abilityScoreImprovement(addLevelStage,maxLevel,4));
+                borderPane.setCenter(abilityScoreImprovement(character, addLevelStage,maxLevel,4));
 //                levelFive(borderPane,continueButton,error,maxLevel);
             }
 
@@ -868,7 +677,7 @@ public class Bard {
                         fourthLevelSpells.remove(fourthLevelSpellsList.getItems().get(i).getText());
                     }
                 }
-                borderPane.setCenter(abilityScoreImprovement(addLevelStage,maxLevel,8));
+                borderPane.setCenter(abilityScoreImprovement(character,addLevelStage,maxLevel,8));
             }
         });
 
@@ -1077,7 +886,7 @@ public class Bard {
         error.setText("");
         updateCharacterHP();
         borderPane.setLeft(leftSetUp());
-        pane.getChildren().add(abilityScoreImprovement(addLevelStage,maxLevel,12));
+        pane.getChildren().add(abilityScoreImprovement(character, addLevelStage,maxLevel,12));
 
     }
 
@@ -1223,7 +1032,7 @@ public class Bard {
         error.setText("");
         updateCharacterHP();
         borderPane.setLeft(leftSetUp());
-        pane.getChildren().add(abilityScoreImprovement(addLevelStage,maxLevel,16));
+        pane.getChildren().add(abilityScoreImprovement(character, addLevelStage,maxLevel,16));
 
     }
 
@@ -1359,7 +1168,7 @@ public class Bard {
                     }
                 }
                 pane.getChildren().clear();
-                pane.getChildren().add(abilityScoreImprovement(addLevelStage,maxLevel,19));
+                pane.getChildren().add(abilityScoreImprovement(character, addLevelStage,maxLevel,19));
             }
         });
 
@@ -1444,27 +1253,6 @@ public class Bard {
 
     }
 
-
-    /**
-     * @param inventory Vector of items
-     * @param item      item you are adding in vector, or adding quantity to already existing item
-     */
-    public void checkAndAddItemQuantity(Vector<Item> inventory, Item item) {
-        int counter = 0;
-        int i;
-        for (i = 0; i < inventory.size(); i++) {
-            if (inventory.get(i).getName().equals(item.getName())) {
-                inventory.get(i).addition(item);
-                counter++;
-                System.out.println("+1 " + inventory.get(i).getName() + " added to inventory | Quantity: " + inventory.get(i).getQuantity());
-            }
-        }
-        if (counter == 0) {
-            inventory.add(item);
-            System.out.println(item.getName() + " added to inventory | Quantity: " + inventory.get(inventory.size() - 1).getQuantity());
-        }
-
-    }
 
     /**
      * Checks if the contents are already in the vector, if yes nothing happens, if no the contents are added to the vector and prints the contents have been added to the vector.
@@ -1786,68 +1574,6 @@ public class Bard {
         return roll;
     }
 
-    /**
-     *
-     * @param abilityScore Takes in the Ability Score
-     * @return Returns the Modifier based on the Ability Score
-     */
-    public static int findAbilityMod(int abilityScore) {
-        Scanner scanner = new Scanner(System.in);
-        Scanner endOfLine = new Scanner(System.in);
-        while(abilityScore<0 || abilityScore>30){
-            System.out.println("Incorrect Ability Score, please enter correct score between 1 and 30");
-            scanner.nextInt();
-            endOfLine.nextLine();
-        }
-
-        if (abilityScore == 1) {
-            return -5;
-        }
-        if (abilityScore == 2 || abilityScore == 3) {
-            return -4;
-        }
-        if (abilityScore == 4 || abilityScore == 5) {
-            return -3;
-        }
-        if (abilityScore == 6 || abilityScore == 7) {
-            return -2;
-        }
-        if (abilityScore == 8 || abilityScore == 9) {
-            return -1;
-        }
-        if (abilityScore == 10 || abilityScore == 11){
-            return 0;
-        }
-        if (abilityScore == 12 || abilityScore ==13) {
-            return 1;
-        }
-        if (abilityScore == 14 || abilityScore ==15) {
-            return 2;
-        }
-        if (abilityScore == 16 || abilityScore ==17) {
-            return 3;
-        }
-        if (abilityScore == 18 || abilityScore ==19) {
-            return 4;
-        }
-        if (abilityScore == 20 || abilityScore ==21) {
-            return 5;
-        }
-        if (abilityScore == 22 || abilityScore ==23) {
-            return 6;
-        }
-        if (abilityScore == 24 || abilityScore ==25) {
-            return 7;
-        }
-        if (abilityScore == 26 || abilityScore ==27) {
-            return 8;
-        }
-        if (abilityScore == 28 || abilityScore ==29) {
-            return 9;
-        }
-        return 10;
-
-    }
 
     /**
      * Code needed to complete the Magical Secrets Feature. Allows the user to select any level spell
